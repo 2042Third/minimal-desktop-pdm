f1719c0d383c88fd5256bdb1d4474cadc3bbd41b
mikeyang <18604713262@163.com>
2025-02-01 23:38:31 -0500

    Wails Basic Vue

---
+++ pdm-desktop/frontend/src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <div id="container" style="--wails-draggable:drag">
+    <img
+        id="logo"
+        style="--wails-draggable:no-drag"
+        alt="Wails logo"
+        src="./assets/images/pdmIcon.svg"
+        draggable="false"
+    />
+    <HelloWorld class="normal-component" style="--wails-draggable:no-drag" />
+  </div>
+</template>
+
+<style>
+
+#container {
+  width: 100%;
+  height: 100%;
+}
+
+#logo {
+  display: block;
+  width: 50%;
+  height: 50%;
+  margin: 10% auto auto ;
+  padding: 0 0 0;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-origin: content-box;
+
+
+  border-radius: 100px;
+
+  /* Prevent interaction */
+  pointer-events: none;
+
+  /* Prevent text/image selection */
+  user-select: none;
+}
+
+normal-component {
+  margin: auto;
+
+  /* Prevent interaction */
+  pointer-events: all;
+
+  /* Prevent text/image selection */
+  user-select: all;
+}
+
+* {
+  -webkit-touch-callout:none;
+  -webkit-user-select:none;
+  -moz-user-select:none;
+  -ms-user-select:none;
+  user-select:none;
+}
+</style>
---
+++ pdm-desktop/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"embed"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:           "pdm-desktop",
+		Width:           1024,
+		Height:          768,
+		CSSDragProperty: "--wails-draggable",
+		CSSDragValue:    "drag",
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		OnStartup: app.startup,
+		Bind: []interface{}{
+			app,
+		},
+		Mac: &mac.Options{
+			TitleBar: &mac.TitleBar{
+				TitlebarAppearsTransparent: true,
+				HideTitle:                  true,
+				HideTitleBar:               false,
+				FullSizeContent:            false,
+				UseToolbar:                 false,
+				HideToolbarSeparator:       true,
+			},
+			Appearance:           mac.NSAppearanceNameDarkAqua,
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  true,
+			About: &mac.AboutInfo{
+				Title:   "My Application",
+				Message: "© 2021 Me",
+				Icon:    []byte("frontend/src/assets/images/pdmIcon.svg"),
+			},
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
